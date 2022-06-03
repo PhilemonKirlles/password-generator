b2f7cdb9098f20d8463dce77c13022bdf0b68953
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-06-03 13:56:16 -0400

    css improvments,js submit form repair, correcting defult to lowercase

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
 # password-generator
+
+![Project Image]()
+
+---
+
+## Table of Contents
+
+- [Description](#description)
+- [How To Use](#how-to-use)
+- [References](#references)
+- [License](#license)
+- [Author Info](#author-info)
+
+---
+
+## Purpose
+Generating random password. 
+
+## Description
+A web-based application for generati
+
+
+
+#### Technologies
+- Google Maps
+- ???
+-???
+
+## Built With
+* HTML5
+* CSS
+* Bulma
+* JavaScript
+* markdown
+---
+
+## How To Use
+explore the website and plan your next beach acticity 
+Visual marking and description of the app features.
+
+#### Installation
+N/A
+
+---
+
+## References
+API
+API
+???
+????
+???
+
+---
+
+## License
+
+bootcamp class License
+
+Copyright (c) [2022] [???sunny side up team???]
+
+---
+
+## Author Info
+-name- [Raymond,Eric]
+-email- []
+-social media- [???]
+-protfolio- [?????]
+
+-name- [Kocaj,Lawrence]
+-email- [?????]
+-social media- [???]
+-protfolio- [?????]
+
+-name- [King,Sarah]
+-email- [?????]
+-social media- [???]
+-protfolio- [?????]
+
+-name- [AbdEllah,Philemon]
+-email- [?????]
+-social media- [???]
+-protfolio- [?????]
+## Website
+https://philemonkirlles.github.io/???/
+
+---
+
+## Contribution
+Name (firstname,alphabetically)
+1-
+2-
+3-
+4-
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 // Assignment code here
 function generate() {
-  const length = document.getElementById("length");
-  const numbers = document.getElementById("numbers");
-  const uppercase = document.getElementById("uppercase");
-  const lowercase = document.getElementById("lowercase");
-  const symbols = document.getElementById("symbols");
-  const form = document.getElementById("stopDefultValue");
-  const password = document.getElementById("password");
+
+const password = document.getElementById("password");
+const length = document.getElementById("length");
+const numbers = document.getElementById("numbers");
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const symbols = document.getElementById("symbols");
+const form = document.getElementById("stopDefultValue");
+  
 
   const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
   const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
@@ -15,9 +17,23 @@ function generate() {
     .concat(arrayFromLowToHigh(58, 64))
     .concat(arrayFromLowToHigh(91, 96))
     .concat(arrayFromLowToHigh(123, 126));
-console.log("SYM ---- ", SYMBOLS_CHAR_CODES)
+
   length.addEventListener("input", syncLength);
   numbers.addEventListener("input", syncLength);
+  uppercase.addEventListener("input", syncLength);
+  symbols.addEventListener("input", syncLength);
+  
+
+  function passwordCopy() {
+    /* Get the text field */
+    var copyPassword = document.getElementById("password");
+
+      /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyPassword.value);
+   /* Alert the copied text */
+   alert("Copied the password: " + copyPassword.value);
+  }
+  
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -27,13 +43,13 @@ console.log("SYM ---- ", SYMBOLS_CHAR_CODES)
     // const lowercase = lowercaseElement.checked;
     const symbols = e.target[3].checked;
     console.log(e);
-    const createdPassword = writePassword(
+    const generatePassword = writePassword(
       length,
       numbers,
       uppercase,
       symbols
     );
-    password.innerText = createdPassword;
+    password.innerText = generatePassword;
   });
 
   // Get references to the #generate element
@@ -42,19 +58,20 @@ console.log("SYM ---- ", SYMBOLS_CHAR_CODES)
   // Write password to the #password input
 
   function writePassword(length, numbers, uppercase, symbols) {
-    console.log(symbols)
+  
     let charCodes = LOWERCASE_CHAR_CODES;
     if (uppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
     if (numbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
     if (symbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES);
-    
+
     const passwordCharacters = [];
+    //for loop: using (Math.random )
     for (let i = 0; i < length; i++) {
       const characterCode =
-      charCodes[Math.floor(Math.random() * charCodes.length)];
+        charCodes[Math.floor(Math.random() * charCodes.length)];
       passwordCharacters.push(String.fromCharCode(characterCode));
     }
-    console.log (passwordCharacters)
+    console.log(passwordCharacters);
     return passwordCharacters.join("");
   }
 
@@ -62,7 +79,6 @@ console.log("SYM ---- ", SYMBOLS_CHAR_CODES)
   function syncLength(e) {
     const value = (e.target.value = value);
     length.value = value;
-    numbers.value = value;
   }
 
   function arrayFromLowToHigh(low, high) {
@@ -72,12 +88,5 @@ console.log("SYM ---- ", SYMBOLS_CHAR_CODES)
     }
     return array;
   }
-
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
-
-  // Add event listener to generate button
-  // generateBtn.addEventListener("click", function () {});
 }
-generate ()
+generate();
