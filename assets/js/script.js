e46beec50bd34a5492ac6bf76e3e419e6d2ce007
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-06-08 10:52:28 -0400

    merge conflect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,61 @@
 // Assignment code here
-const length = document.getElementById ("length")
-const numbers = document.getElementById ("numbers")
-const form = document.getElementById ("stopDefultValue")
+function generate() {
+  // get DOM Elements:
+  const password = document.getElementById("password");
+  const length = document.getElementById("length");
+  const numbers = document.getElementById("numbers");
+  const uppercase = document.getElementById("uppercase");
+  // const lowercase = document.getElementById("lowercase");
+  const symbols = document.getElementById("symbols");
+  const form = document.getElementById("stopDefultValue");
+   //construct ascii-char codes 
+  const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
+  const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+  const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+  const SYMBOLS_CHAR_CODES = arrayFromLowToHigh(33, 47)
+    .concat(arrayFromLowToHigh(58, 64))
+    .concat(arrayFromLowToHigh(91, 96))
+    .concat(arrayFromLowToHigh(123, 126));
+ //  length value function
+  length.addEventListener("input", syncLength);
+  numbers.addEventListener("input", syncLength);
+  uppercase.addEventListener("input", syncLength);
+  symbols.addEventListener("input", syncLength);
 
-length.addEventListener('input', syncLength)
+          // To Be Continued:
+             // Copy Password
+          //function passwordCopy() {
+          //   /* Get the text field */
+          //   var copyPassword = document.getElementById("password");
 
-form.addEventListener ('submit', e => {e.preventDefault()
-  const password = generatePassword (length,numbers,uppercase,lowercase, Symbols)
-})
+          //   /* Copy the text inside the text field */
+          //   navigator.clipboard.writeText(copyPassword.value);
+          //   /* Alert the copied text */
+          //   alert("Copied the password: " + copyPassword.value);
+          // }
+//form event listeners, and passing them to function
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const length = e.target[0].value;
+    const numbers = e.target[2].checked;
+    const uppercase = e.target[1].checked;
+    // const lowercase = lowercaseElement.checked;
+    const symbols = e.target[3].checked;
+    console.log(e);
+    const generatePassword = writePassword(length, numbers, uppercase, symbols);
+    password.innerText = generatePassword;
+  });
 
-// length value function
-function syncLength(e) {
-  const value = e.target.value = value
-  length.value = value
-}
+  
+  // generateBtn = document.querySelector("#generate");
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
+  // generate password to the #password textarea and concatenate the values 
+  function writePassword(length, numbers, uppercase, symbols) {
+    let charCodes = LOWERCASE_CHAR_CODES;
+    if (uppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+    if (numbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
+    if (symbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES);
 
   passwordText.value = password;
 
