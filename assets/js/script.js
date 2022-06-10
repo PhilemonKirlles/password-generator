2f6c98e363ffe731860cf39293e85e527fe81af6
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-06-10 19:05:55 -0400

    adding css grid to control flow and responsivness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Assignment code here
+
 function generate() {
-   // get DOM Elements:
+
+  // get DOM Elements:
   const password = document.getElementById("password");
   const length = document.getElementById("length");
   const numbers = document.getElementById("numbers");
@@ -8,7 +10,8 @@ function generate() {
   // const lowercase = document.getElementById("lowercase");
   const symbols = document.getElementById("symbols");
   const form = document.getElementById("stopDefultValue");
-  //construct ascii-char codes 
+  
+  //construct ascii-char codes
   const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
   const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
   const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
@@ -16,24 +19,31 @@ function generate() {
     .concat(arrayFromLowToHigh(58, 64))
     .concat(arrayFromLowToHigh(91, 96))
     .concat(arrayFromLowToHigh(123, 126));
- //  length value function
+
   length.addEventListener("input", syncLength);
   numbers.addEventListener("input", syncLength);
   uppercase.addEventListener("input", syncLength);
   symbols.addEventListener("input", syncLength);
 
+  //  length value function
+
+  function syncLength(e) {
+    const value = (e.target.value = value);
+    length.value = value;
+  }
   // To Be Continued:
   //plan:
   //function passwordCopy() {
-  //   /* Get the text field 
+  //   /* Get the text field
   //   var copyPassword = document.getElementById("password");
 
-  //   Copy the text inside the text field 
+  //   Copy the text inside the text field
   //   navigator.clipboard.writeText(copyPassword.value);
-  //   Alert  copied text 
+  //   Alert  copied text
   //   alert("Copied the password: " + copyPassword.value);
   // }
-//form event listeners, and passing them to function
+  //form event listeners, and passing them to function
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     const length = e.target[0].value;
@@ -42,14 +52,14 @@ function generate() {
     // const lowercase = lowercaseElement.checked;
     const symbols = e.target[3].checked;
     console.log(e);
+
     const generatePassword = writePassword(length, numbers, uppercase, symbols);
     password.innerText = generatePassword;
   });
 
-  // Get references to the #generate element
   // var generateBtn = document.querySelector("#generate");
 
-  // generate password to the #password textarea and concatenate the values 
+  // generate password to the #password textarea and concatenate the values
   function writePassword(length, numbers, uppercase, symbols) {
     let charCodes = LOWERCASE_CHAR_CODES;
     if (uppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
@@ -63,18 +73,11 @@ function generate() {
         charCodes[Math.floor(Math.random() * charCodes.length)];
       passwordCharacters.push(String.fromCharCode(characterCode));
     }
+
     console.log(passwordCharacters);
     return passwordCharacters.join("");
   }
 
-  // length value function
-  
-  function syncLength(e) {
-    length.value = value;
-    const value = (e.target.value = value);
-    
-  }
-  console.log (e);
   function arrayFromLowToHigh(low, high) {
     const array = [];
     for (let i = low; i <= high; i++) {
